389034dc28014e310dbf0bdaff9547b51ae392e1
LeylaNovini <novil040@newschool.edu>
2016-09-14 14:58:23 -0400

    image upload

--- alpacacounter.py
+++ alpacacounter.py
@@ -4,40 +4,57 @@ from email.MIMEText import MIMEText
 from email.mime.image import MIMEImage
 from cStringIO import StringIO
 from email.generator import Generator
+import time
 
-fromaddr = 'youreemail@gmail.com'
-toaddr = 'anotheremail@gmail.com'
-msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = 'Fun Facts About Alpacas'
 
-body = 'Have you ever wondered about the fascinating life of an alpaca ? They are truely extrordinary animals. The alpaca has fur that is waterproof and fire resistant. The alpaca also has a variation of 22 different colors of fur.'
-msg.attach(MIMEText(body,'plain'))
+# Define these once; use them twice!
+strFrom = 'you@email.com'
+strTo = 'another@email.com'
 
-fp = open('images/alpaca.png', 'rb')
+# Create the root message and fill in the from, to, and subject headers
+msgRoot = MIMEMultipart('related')
+msgRoot['Subject'] = 'Fun Facts About Alpacas'
+msgRoot['From'] = strFrom
+msgRoot['To'] = strTo
+msgRoot.preamble = 'Have you ever wondered about the fascinating life of an alpaca ? They are truely extrordinary animals. The alpaca has fur that is waterproof and fire resistant. The alpaca also has a variation of 22 different colors of fur.'
+
+# Encapsulate the plain and HTML versions of the message body in an
+# 'alternative' part, so message agents can decide which they want to display.
+msgAlternative = MIMEMultipart('alternative')
+msgRoot.attach(msgAlternative)
+
+msgText = MIMEText('This is the alternative plain text message.')
+msgAlternative.attach(msgText)
+
+# We reference the image in the IMG SRC attribute by the ID we give it below
+msgText = MIMEText('Have you ever wondered about the fascinating life of an alpaca ? They are truely extrordinary animals. The alpaca has fur that is waterproof and fire resistant. The alpaca also has a variation of 22 different colors of fur.', 'html')
+msgAlternative.attach(msgText)
+
+# This example assumes the image is in the current directory
+fp = open('alpaca.png', 'rb')
 msgImage = MIMEImage(fp.read())
 fp.close()
 
+# Define the image's ID as referenced above
+msgImage.add_header('Content-ID', '<image1>')
+msgRoot.attach(msgImage)
 
-for file in images:
-	with open(file, 'alpaca') as fp:
-    	img = MIMEImage(fp.read())
-msg.attach(img)
+def imagecount(n):
+    if n > 0: 
+        print "you have "+str(n)+" picture(s) of alpacas"
+    else: 
+        print "you have no pictures of alpacas"
 
-# def imagecount(n):
-#     if n > 0: 
-#         print "you have "+str(n)+" picture(s) of alpacas"
-#     else: 
-#         print "you have no pictures of alpacas"
+    imagecount(newimage) #call emailcount function and pass value newmails as agrument
+    time.sleep(5) 
 
-wordlist = body.split()
+# wordlist = msgRoot.split()
 
-wordfreq = []
-for w in wordlist:
-    wordfreq.append(wordlist.count(w))
+# wordfreq = []
+# for w in wordlist:
+#     wordfreq.append(wordlist.count(w))
 
-print("The word alpaca appears " + str(wordfreq) + " times in this email")
+# print("The word alpaca appears " + str(wordfreq) + " times in this email")
 
 # b = email.message_from_string(a)
 # if b.is_multipart():
@@ -55,7 +72,7 @@ print("The word alpaca appears " + str(wordfreq) + " times in this email")
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(fromaddr, '*****')
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
+server.login(strFrom, '******')
+text = msgRoot.as_string()
+server.sendmail(strFrom, strTo, text)
 server.quit()
