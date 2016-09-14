a81f7d19be504df9b1e8670ecd9912706ff11bd3
LeylaNovini <novil040@newschool.edu>
2016-09-14 15:01:34 -0400

    update out comments

--- alpacacounter.py
+++ alpacacounter.py
@@ -56,20 +56,6 @@ def imagecount(n):
 
 # print("The word alpaca appears " + str(wordfreq) + " times in this email")
 
-# b = email.message_from_string(a)
-# if b.is_multipart():
-#     for part in b.walk():
-#         ctype = part.get_content_type()
-#         cdispo = str(part.get('Content-Disposition'))
-
-#         # skip any text/plain (txt) attachments
-#         if ctype == 'text/plain' and 'attachment' not in cdispo:
-#             body = part.get_payload(decode=True)  # decode
-#             break
-# # not multipart - i.e. plain text, no attachments, keeping fingers crossed
-# else:
-#     body = b.get_payload(decode=True)
-
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(strFrom, '******')
