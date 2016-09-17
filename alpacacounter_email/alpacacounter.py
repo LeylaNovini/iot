4e87b16eec09ba371d3cae732b82c17ca3ecfe71
LeylaNovini <novil040@newschool.edu>
2016-09-17 13:36:51 -0400

    added two more arguments to send in email

--- alpacacounter_email/alpacacounter.py
+++ alpacacounter_email/alpacacounter.py
@@ -6,7 +6,6 @@ from cStringIO import StringIO
 from email.generator import Generator
 import time
 
-
 # emails
 strFrom = 'your@email.com'
 strTo = 'another@email.com'
@@ -18,15 +17,12 @@ msgRoot['From'] = strFrom
 msgRoot['To'] = strTo
 msgRoot.preamble = 'Have you ever wondered about the fascinating life of an alpaca ? They are truely extrordinary animals. The alpaca has fur that is waterproof and fire resistant. The alpaca also has a variation of 22 different colors of fur.'
 
-# Encapsulate the plain and HTML versions of the message body in an
-# 'alternative' part, so message agents can decide which they want to display.
 msgAlternative = MIMEMultipart('alternative')
 msgRoot.attach(msgAlternative)
 
 msgText = MIMEText('This is the alternative plain text message.')
 msgAlternative.attach(msgText)
 
-# We reference the image in the IMG SRC attribute by the ID we give it below
 msgText = MIMEText('Have you ever wondered about the fascinating life of an alpaca ? They are truely extrordinary animals. The alpaca has fur that is waterproof and fire resistant. The alpaca also has a variation of 22 different colors of fur.', 'html')
 msgAlternative.attach(msgText)
 
@@ -35,7 +31,6 @@ fp = open('alpaca.png', 'rb')
 fp1 = open('3cuties.png', 'rb')
 fp2 = open ('alpacahair.png', 'rb')
 fp3 = open('group.png', 'rb')
-
 
 msgImage = MIMEImage(fp.read())
 msgImage1 = MIMEImage(fp1.read())
@@ -46,7 +41,6 @@ fp1.close()
 fp2.close()
 fp3.close()
 
-# Define the image's ID as referenced above
 msgImage.add_header('Content-ID', '<image1>')
 msgRoot.attach(msgImage)
 
@@ -59,22 +53,26 @@ msgRoot.attach(msgImage2)
 msgImage3.add_header('Content-ID', '<image4>')
 msgRoot.attach(msgImage3)
 
+newMessage = None
+#count the word alpaca
+body = 'Have you ever wondered about the fascinating life of an alpaca ? They are truely extrordinary animals. The alpaca has fur that is waterproof and fire resistant. The alpaca also has a variation of 22 different colors of fur.  '
+body2 = 'alpaca'
+body3 = body.count(body2)
 
-body = 'Have you ever wondered about the fascinating life of an alpaca ? They are truely extrordinary animals. The alpaca has fur that is waterproof and fire resistant. The alpaca also has a variation of 22 different colors of fur.'
-wordlist = body.split()
+body4 = "The word alpaca appears " +str(body3)+ " times in this email.  "
+print body4
 
-wordfreq = []
-for w in wordlist:
-    wordfreq.append(wordlist.count(w))
-
-print("The word alpaca appears " + str(wordfreq) + " times in this email")
-
+#count the images
 newimage = [msgImage, msgImage1, msgImage2, msgImage3]
-
 finalimage = len(newimage)
 
-print "you have " +str(finalimage)+ " pictures of alpacas"
+body5 = "You have " +str(finalimage)+ " pictures of alpacas.  "
+print body5
 
+newMessage = body + body4 + body5
+
+msgText2 = MIMEText(str(newMessage))
+msgAlternative.attach(msgText2)
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
